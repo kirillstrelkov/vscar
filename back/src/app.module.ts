77d93611eb6739e76058f5a6f96a03575c80dd61
Kirill Strelkov <kirill.strelkov@github.com>
2020-09-15 23:41:24 +0200

    add redirect back from / to /api

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017/test'), CarsModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
