9e9dd32fc8f583ce1ba53e5cdb6990410a16f73b
Kirill Strelkov <kirill.strelkov@github.com>
2020-10-10 09:13:35 +0200

    DATABASE_URI for back config

--- back/config/configuration.ts
+++ back/config/configuration.ts
@@ -1,7 +1,7 @@
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
     database: {
-        uri: `mongodb://${process.env.DATABASE_HOST || 'localhost'}:${parseInt(process.env.DATABASE_PORT, 10) || 27017}/${process.env.DATABASE_NAME || 'carvs'}`,
+        uri: process.env.DATABASE_URI || `mongodb://${process.env.DATABASE_HOST || 'localhost'}:${parseInt(process.env.DATABASE_PORT, 10) || 27017}/${process.env.DATABASE_NAME || 'carvs'}`,
         // TODO: cleanup
         host: process.env.DATABASE_HOST || 'localhost',
         port: parseInt(process.env.DATABASE_PORT, 10) || 27017,
