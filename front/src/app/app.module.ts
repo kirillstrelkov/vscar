01489a4b6eccfa455d6c62815f9a49653332478d
Kirill Strelkov <kirill.strelkov@gmail.com>
2021-04-01 19:57:36 +0200

    refactor: organize imports

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,34 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
-
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { CarDetailsComponent } from './car-details/car-details.component';
-import { SearchComponent } from './search/search.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { CarListComponent } from './car-list/car-list.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarDetailsComponent } from './car-details/car-details.component';
+import { CarListComponent } from './car-list/car-list.component';
 import { CompareBarComponent } from './compare-bar/compare-bar.component';
 import { CompareListComponent } from './compare-list/compare-list.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchComponent } from './search/search.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatTooltipModule } from '@angular/material/tooltip';
+
+
+
+
 
 @NgModule({
   declarations: [
