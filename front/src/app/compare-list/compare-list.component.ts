5f8f3aed5e19f2a5b3c5364b0ae0d40aa3fe4663
Kirill Strelkov <kirill.strelkov@gmail.com>
2023-08-01 22:17:06 +0200

    front: remove fixed columns from compare list

--- front/src/app/compare-list/compare-list.component.ts
+++ front/src/app/compare-list/compare-list.component.ts
@@ -41,7 +41,7 @@ export class CompareListComponent implements AfterViewInit {
         const attrs = attrsToAdd.concat(cars[0].attributes.map((c) => c.name));
         let newData = [];
         for (const attr of attrs) {
-          if (attrsToSkip.has(attr)) {
+          if (attrsToSkip.has(attr) || attr.indexOf('fixed') !== -1) {
             continue;
           }
 
