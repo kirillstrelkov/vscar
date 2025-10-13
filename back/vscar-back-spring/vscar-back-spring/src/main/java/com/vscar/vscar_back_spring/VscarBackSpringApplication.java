9f9ec563b6a4ddbdfa701fbf43a929c0b86c12ea
Kirill Strelkov <7988855+kirillstrelkov@users.noreply.github.com>
2025-10-13 11:28:47 +0200

    feat: initi spring

---
+++ back/vscar-back-spring/vscar-back-spring/src/main/java/com/vscar/vscar_back_spring/VscarBackSpringApplication.java
@@ -0,0 +1,13 @@
+package com.vscar.vscar_back_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VscarBackSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VscarBackSpringApplication.class, args);
+	}
+
+}
---
+++ back/vscar-back-spring/vscar-back-spring/src/test/java/com/vscar/vscar_back_spring/VscarBackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vscar.vscar_back_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VscarBackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
