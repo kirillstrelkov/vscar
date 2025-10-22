0a2519b01a66ef6686315bf343a7550ce866ca39
Kirill Strelkov <7988855+kirillstrelkov@users.noreply.github.com>
2025-10-22 11:55:52 +0200

    fix: reset_db

--- back/vscar-back-nestjs/reset_db.ts
+++ back/vscar-back-nestjs/reset_db.ts
@@ -1,49 +1,52 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { CarSchema } from "./src/cars/schemas/car.schema";
-import configuration from "./config/configuration";
+import { CarSchema } from './src/cars/schemas/car.schema';
+import configuration from './config/configuration';
 
+import * as os from 'os';
 
 async function load_json_to_db(path: string) {
+  const db_config = configuration().database;
+  const uri = db_config.uri;
+  console.log(`Loading ${path} to db ${uri}`);
 
-    const db_config = configuration().database;
-    const uri = db_config.uri;
-    console.log(`Loading ${path} to db ${uri}`);
+  const mongoose = require('mongoose');
+  mongoose.connect(uri);
 
-    const mongoose = require('mongoose');
-    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  const Car = mongoose.model('Cars', { CarSchema });
+  await Car.cleanIndexes();
 
-    // TODO: improve
-    const Car = mongoose.model('Cars', { CarSchema });
-    await Car.cleanIndexes();
+  await Car.deleteMany({}).then((docs: any) => {
+    console.log('Removed:', docs);
+  });
 
-    await Car.deleteMany({}).then((docs: any) => { console.log('Removed:', docs); });
+  const json = require(path);
+  await Car.collection.insertMany(json).then((docs: any) => {
+    delete docs.insertedIds;
+    console.log('Added:', docs);
+  });
 
-    const json = require(path);
-    await Car.collection.insertMany(json).then((docs: any) => {
-        console.log('Added:', docs.result);
-        mongoose.connection.close();
-    });
+  await Car.createIndexes();
+  console.log('Indices ensured.');
+
+  mongoose.connection.close();
 }
 
 async function main() {
-    const input_archive = "../../db/db.zip";
+  const input_archive = '../../db/db.zip';
 
-    const tempDirectory = require('temp-dir');
+  const output_dir = os.tmpdir();
+  try {
+    console.log(`Extracting ${input_archive} to ${output_dir}`);
 
-    const output_dir = tempDirectory;
-    try {
-        console.log(`Extracting ${input_archive} to ${output_dir}`);
+    const extract = require('extract-zip');
+    await extract(input_archive, { dir: output_dir });
 
-        const extract = require("extract-zip");
-        await extract(input_archive, { dir: output_dir });
+    console.log('Done\n');
+  } catch (err) {
+    console.log(err);
+  }
 
-        console.log("Done\n");
-
-    } catch (err) {
-        console.log(err);
-    }
-
-    load_json_to_db(`${output_dir}/car.json`);
+  load_json_to_db(`${output_dir}/car.json`);
 }
 
 main();
